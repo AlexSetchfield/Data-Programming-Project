669c28f4bcdd3404b84e54ad1be19ab4d14e3dab
hyhy8181994 <hyhy8181994@gmail.com>
2016-11-16 12:27:35 +0000

    Add files via upload

---
+++ ghffds.R
@@ -0,0 +1,25 @@
+
+A<-2.5
+maxrate<-0.055
+l<-35
+t<-seq(0,200, by=0.5)
+
+
+t<-50
+y<-A*exp(-exp(maxrate*exp(1)*(l-t)/A+1))
+plot(t,y)
+
+t<-NULL
+
+t<-(A/(maxrate*exp(1)))*log(exp(1),log(exp(1),y/A))-l-1
+
+
+t<-(A/(maxrate*exp(1)))*
+
+  
+log(exp(1),log(exp(1),y/A))-l-1
+
+
+log(-log(y/A))
+
+
