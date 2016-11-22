a1e05b5811022bcb5df1a8f58fa16ab2da9ec709
setchfielda <ats527@york.ac.uk>
2016-11-22 17:16:47 +0000

    Update Combined Lysis Buffer Calculations + Documentation.R

--- Combined Lysis Buffer Calculations + Documentation.R
+++ Combined Lysis Buffer Calculations + Documentation.R
@@ -23,12 +23,12 @@
 #' PMSF.conc <- 1
 #' volume <- 1000
 #'
-#' masscalc(TrisHCl.conc, NaCl.conc, DTT.conc, PMSF.conc, volume)
+#' lbmasscalc(TrisHCl.conc, NaCl.conc, DTT.conc, PMSF.conc, volume)
 #'
-#' masscalc(TrisHCl.conc = 50, NaCl.con = 200, DTT.conc = 5, PMSF.conc = 1, volume = 1000)
+#' lbmasscalc(TrisHCl.conc = 50, NaCl.con = 200, DTT.conc = 5, PMSF.conc = 1, volume = 1000)
 #' 
 #' @export
-masscalc <- function(TrisHCl.conc, NaCl.conc, DTT.conc, PMSF.conc, volume) {
+lbmasscalc <- function(TrisHCl.conc, NaCl.conc, DTT.conc, PMSF.conc, volume) {
   TrisHClmol.L <- (TrisHCl.conc / 1000)
   NaClmol.L <- (NaCl.conc / 1000)
   DTTmol.L <- (DTT.conc / 1000)
@@ -53,7 +53,7 @@ DTT.conc <- 5 # Enter concentration of DTT (mM) required
 PMSF.conc <- 1 # Enter concentration of PMSF (mM) required
 volume <- 1000 # Enter final volume (ml) required
 
-masscalc <- function(TrisHCl.conc, NaCl.conc, DTT.conc, PMSF.conc, volume) {
+lbmasscalc <- function(TrisHCl.conc, NaCl.conc, DTT.conc, PMSF.conc, volume) {
   TrisHClmol.L <- (TrisHCl.conc / 1000)
   NaClmol.L <- (NaCl.conc / 1000)
   DTTmol.L <- (DTT.conc / 1000)
@@ -70,4 +70,4 @@ masscalc <- function(TrisHCl.conc, NaCl.conc, DTT.conc, PMSF.conc, volume) {
   mass.list <- list("Tris Mass (g)" = TrisHCl.mass, "NaCl Mass (g)" = NaCl.mass, "DTT Mass (g)" = DTT.mass, "PMSF Mass (g)" = PMSF.mass)
   return(mass.list)
 }
-masscalc(TrisHCl.conc, NaCl.conc, DTT.conc, PMSF.conc, volume)
+lbmasscalc(TrisHCl.conc, NaCl.conc, DTT.conc, PMSF.conc, volume)
