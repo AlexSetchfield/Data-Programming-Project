5d847e1273517074cfe048df04205b4129dcbbd0
alicepearcy <alice.pearcy@live.co.uk>
2016-11-23 19:19:56 +0000

    Update testthat.R

--- testthat.R
+++ testthat.R
@@ -2,8 +2,8 @@ library(testthat)
 library(workflow)
 
 # DNAaa() test
-context("DNAaatests")
-test_that("DNAaatest", {
+context("DNAaatestscases")
+test_that("DNAaa test cases", {
   expect_that(DNAaa("GTTCAT"), prints_text("gene length = 6 bp", "protein sequence = VH", "number of amino acid= 2"))
   expect_that(DNAaa("gttcat"), prints_text("gene length = 6 bp", "protein sequence = VH", "number of amino acid= 2"))
   expect_that(DNAaa("123gttcat"), prints_text("gene length = 6 bp", "protein sequence = VH", "number of amino acid= 2"))
@@ -11,7 +11,7 @@ test_that("DNAaatest", {
 
 # mw() test
 context("mwtests")
-test_that("mwtest", {
+test_that("mw test cases", {
   expect_that(mw("VH"), prints_text("aa sequence length =  2", "Molecular Weight prediction = 0.29 kDa"))
   expect_that(mw("vh"), prints_text("aa sequence length =  2", "Molecular Weight prediction = 0.29 kDa"))
   expect_that(mw("123vh"), prints_text("aa sequence length =  2", "Molecular Weight prediction = 0.29 kDa"))
@@ -19,13 +19,13 @@ test_that("mwtest", {
 
 # gram.calc() test
 context("gram.calctests")
-test_that("gram.calctest", {
+test_that("gram.calc test output", {
   expect_equivalent(gram.calc("DTT", 5, 25, 58.44), "DTT = 0.007305 g")
 })
 
 # OD600() test
 context("OD600tests")
-test_that("OD600test", {
+test_that("OD600 test output", {
   expect_that(OD600(1.0, accurate = T, 100, 0.3, 200, 0.5), prints_text("Need 6 Hour 40 min to reach OD 1"))
   expect_that(OD600(1.0, accurate = F, 100), prints_text("Need 12 Hour 55 min to reach OD 1"))
   expect_that(OD600(1.0, 100), prints_text("Need 12 Hour 55 min to reach OD 1"))
@@ -33,7 +33,7 @@ test_that("OD600test", {
 
 # lbmasscalc() test
 context("lbmasscalctests")
-test_that("lbmasscalctest", {
+test_that("lbmasscalc test output", {
   masses <- lbmasscalc(TrisHCl.conc = 50, NaCl.conc = 200, DTT.conc = 5, PMSF.conc = 1, volume = 1000)
   expect_that(masses$"Tris Mass (g)", equals(6.057))
   expect_that(masses$"NaCl Mass (g)", equals(11.688))
