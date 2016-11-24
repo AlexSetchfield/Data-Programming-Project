8aa1b4cee5e23b195d6314ff4c22b557083d41db
alicepearcy <alice.pearcy@live.co.uk>
2016-11-24 14:40:42 +0000

    Update conc calc.R

--- conc calc.R
+++ conc calc.R
@@ -4,28 +4,28 @@
 #' volume wanted and molecular weight of chemicals
 #' 
 #'@param chem name of chemical to be calculated
-#' @param conc_wantedmMl concentration required of the chemical used
+#' @param conc.wantedmMl concentration required of the chemical used
 #' @param volml volume of the solution wanted
 #' @param formweightgM molecular weight of the chemical used
 #' 
 #' @return weight (in g) of the chemical specified required to make the solution to specified concentration
 #' 
 #' @examples 
-#' gram_calc("DTT", 5, 25, 154.25)
+#' gram.calc("DTT", 5, 25, 154.25)
 #' 
 #' chem <- "DTT"
-#' conc_wantedmML <- 5
+#' conc.wantedmML <- 5
 #' volml <- 25
 #' formweightgM <- 154.25
-#' gram_calc(chem, conc_wantedmML, volml, formweightgM)
+#' gram.calc(chem, conc.wantedmML, volml, formweightgM)
 #' 
 #' @export
 chem <- "DTT"
-conc_wantedmML <- 5
+conc.wantedmML <- 5
 volml <- 24
 formweightgM <- 154.25
-gram_calc <- function(chem, conc_wantedmML, volml, formweightgM){
-  grams <- (conc_wantedmML/1000) * (volml/1000) * formweightgM
+gram.calc <- function(chem, conc.wantedmML, volml, formweightgM){
+  grams <- (conc.wantedmML/1000) * (volml/1000) * formweightgM
   return(paste(chem, "=", grams, "g"))
   }
 
