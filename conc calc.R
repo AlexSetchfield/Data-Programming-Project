c32e763efed72d31b065932216c7a6b8d30dbe42
setchfielda <ats527@york.ac.uk>
2016-11-25 10:04:16 +0000

    Update conc calc.R

--- conc calc.R
+++ conc calc.R
@@ -20,14 +20,7 @@
 #' gram.calc(chem, conc.wantedmML, volml, formweightgM)
 #' 
 #' @export
-chem <- "DTT"
-conc.wantedmML <- 5
-volml <- 24
-formweightgM <- 154.25
 gram.calc <- function(chem, conc.wantedmML, volml, formweightgM){
   grams <- (conc.wantedmML/1000) * (volml/1000) * formweightgM
   return(paste(chem, "=", grams, "g"))
-  }
-
-
-
+}
